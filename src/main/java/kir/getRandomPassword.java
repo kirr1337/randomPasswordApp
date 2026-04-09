d95e23cb02a4147f46537ec3129bbb84753a90a0
kirr1337 <cheatengine080@gmail.com>
2026-04-09 17:34:18 +0700

    Добавил пользовательский ввод!

--- src/main/java/kir/getRandomPassword.java
+++ src/main/java/kir/getRandomPassword.java
@@ -1,28 +1,36 @@
 package kir;
 
+import java.util.InputMismatchException;
 import java.util.Random;
+import java.util.Scanner;
 
 public class getRandomPassword {
     Random random = new Random();
-    char[]chars = {'1','2','3','4','5','6','7','8','9','0',
+    Scanner input = new Scanner(System.in);
+    int lengthOfPassword;
+    char[]chars = {
+            '1','2','3','4','5','6','7','8','9','0',
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
             'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
             'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E',
             'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
             'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '#',
-            '!', '@', '&', '*', '^', '%', '$', '~', '`'
+            '!', '@', '&', '*', '^', '%', '$', '~'
     };
     public String password = String.valueOf(chars[random.nextInt(0, chars.length)]);
-    void getPassword(int lengthOfPassword){
-        for (int i = 0; i < lengthOfPassword; i++) {
-            password += chars[random.nextInt(0, chars.length)];
+    void getPassword(){
+        System.out.println("Введите длинну пароля: ");
+        try {
+            lengthOfPassword = input.nextInt();
+            if (lengthOfPassword <= 0){
+                System.out.println("Введите пожалуйста число больше нуля ):");
+                return;
+            }
         }
-        System.out.println(password);
-    }
-
-    /* Делаем перегрузку метода на случай если значение параметра не будет выставлено */
-    void getPassword(){ // a
-        for (int i = 0; i < 10; i++) { // ака ставим значение по умолчанию (10 символов)
+        catch (InputMismatchException e){
+            System.out.println("Ошибка!\nВы ввели не цифры!");
+        }
+        for (int i = 0; i < lengthOfPassword; i++) {
             password += chars[random.nextInt(0, chars.length)];
         }
         System.out.println(password);
